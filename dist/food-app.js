b3316ecd76529b018669c6a91fab4e17d54ea4b9
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-12-06 11:04:51 +0900

    ok

--- dist/food-app.js
+++ dist/food-app.js
@@ -4,16 +4,27 @@ class Score {
 class Food {
     constructor(element) {
         this.element = element;
-        element.addEventListener('click', this.clickEventHandler);
+        element.addEventListener('click', this.clickEventHandler.bind(this));
     }
     clickEventHandler() {
         this.element.classList.toggle('food--active');
     }
 }
 class Foods {
+    get activeElements() {
+        this._activeElements = [];
+        this.elements.forEach(element => {
+            if (element.classList.contains('food--active')) {
+                this._activeElements.push(element);
+            }
+        });
+        return this._activeElements;
+    }
     constructor() {
         this.elements = document.querySelectorAll('.food');
+        this._activeElements = [];
         this.elements.forEach(element => {
+            new Food(element);
         });
     }
 }
